adaabd6776827001cf61dbc0ca088fafefdc7d80
Nigel Leck <nigel@stsoftware.com.au>
2025-11-17 08:13:07 +1100

    Enable verbose logging for NEAT-AI discovery and update run script to execute discovery library

--- discovery/run.sh
+++ discovery/run.sh
@@ -8,6 +8,11 @@ cd "${REPO_ROOT}"
 : "${DISCOVERY_TARGET_UUID:=auto}"
 export DISCOVERY_TARGET_UUID
 
+# export DISCOVERY_TARGET_UUID="3b8b89e7-5ceb-4383-a419-0f11c5541ddb"
+
+export NEAT_AI_DISCOVERY_VERBOSE=1  
+(cd ../NEAT-AI-Discovery&& ./scripts/runlib.sh)
+
 deno run \
   --v8-flags=--max-old-space-size=12288 \
   --allow-read \
